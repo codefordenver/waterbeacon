14940c0f9db1247abfa2d7f09d1f2e12d557fc12
rowvigor <moses@rowvigor.com>
2018-12-15 07:07:00 -0700

    no message

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -4,4 +4,5 @@ from api.v1 import views
 
 urlpatterns = [
     url(r'^data/', views.nodeData.as_view(), name='node-data'),
+    #url(r'^subscribe/', views.subscribe.as_view(), name='subscribe'),
 ]
