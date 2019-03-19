f789f3239bf184209e46c0ffddff658e5b73e23c
Brandon Keao <Brandon.Keao@gmail.com>
2019-03-18 20:05:39 -0600

    created DefaultD3 placeholder in React architecture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DefaultD3 from './components/DefaultD3';
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to RELOAD.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <DefaultD3/>
         </header>
       </div>
     );
---
+++ frontend/src/components/DefaultD3.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export class DetaultD3 extends React.Component {
+  render () {
+    return (
+      <div>
+        <p>
+          Edit <code>src/App.js</code> and save to RELOAD.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    );
+  }
+}
+
+export default DetaultD3;
