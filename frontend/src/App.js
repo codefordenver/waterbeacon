c5aeaf86fc8b0e6d7a4750baf719916f7832ab4b
Brandon Keao <Brandon.Keao@gmail.com>
2019-04-08 22:49:08 -0600

    Rendered US map to page, broken headers without bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import DefaultD3 from './components/DefaultD3';
+import WbHeader from './components/WbHeader';
 
 class App extends Component {
   render() {
     return (
-
       <div className="App">
-
-        <header className="App-header">
-          <DefaultD3/>
-        </header>
+        <WbHeader />
+        <svg width="960" height="600">
+          <DefaultD3 width={960} height={600} />
+        </svg>
       </div>
     );
   }
--- frontend/src/components/DefaultD3.js
+++ frontend/src/components/DefaultD3.js
@@ -1,22 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+import * as topojson from 'topojson';
 
-export class DetaultD3 extends React.Component {
+class DetaultD3 extends Component {
+
+  constructor(props) {
+    console.log("constructor()")
+    super(props)
+    this.state = {
+      topologyData: null,
+      unemploymentData: null
+    }
+  }
+
+  componentWillMount() {
+    console.log("componentWillMount()")
+
+    Promise.all(
+      [d3.json("https://d3js.org/us-10m.v1.json"),
+      d3.tsv("../static/unemployment.tsv")]
+    ).then( ([topologyData, unemploymentData]) => {
+          this.setState({
+            topologyData,
+            unemploymentData
+          })
+    }).catch(err => console.log('Error loading or parsing data.'))
+
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate()")
+
+    const svg = d3.select(this.refs.anchor),
+      { width, height } = this.props;
+
+    const path = d3.geoPath();
+    const unemployment = d3.map();
+
+    unemployment.set(this.state.unemploymentData.id, +this.state.unemploymentData.rate);
+
+    const x = d3.scaleLinear()
+        .domain([1, 10])
+        .rangeRound([400, 860]);
+
+    const color = d3.scaleThreshold()
+        .domain(d3.range(2, 10))
+        .range(d3.schemeBlues[9]);
+
+    const g = svg.append("g")
+        .attr("class", "key")
+        .attr("transform", "translate(0,40)");
+
+    g.selectAll("rect")
+      .data(color.range().map(function(d) {
+          d = color.invertExtent(d);
+          if (d[0] == null) d[0] = x.domain()[0];
+          if (d[1] == null) d[1] = x.domain()[1];
+          return d;
+        }))
+
+    const usStates = topojson.feature(this.state.topologyData, this.state.topologyData.objects.states).features
+    const usStatePaths = topojson.mesh(this.state.topologyData, this.state.topologyData.objects.states, function(a, b) { return a !== b; });
+
+    svg.append("g")
+      .attr("class", "states")
+      .selectAll("path")
+      .data(usStates)
+      .enter().append("path")
+        .attr("fill", function(d) { return color(d.rate = unemployment.get(d.id)); })
+        .attr("d", path)
+      .append("title")
+        .text(function(d) { return d.rate + "%"; })
+      .attr("d", path);
+
+
+    svg.append("path")
+      .attr("class", "state-borders")
+      .attr("d", path(usStatePaths));
+
+  }
+
   render () {
-    return (
-      <div>
-        <p>
-          Edit <code>src/App.js</code> and save to RELOAD.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </div>
-    );
+    console.log("render()")
+    const { topologyData, unemploymentData } = this.state;
+
+    if(!topologyData || !unemploymentData) {
+       return null;
+    }
+
+    return <g ref="anchor" />;
   }
 }
 
