c795664108431a1a3904091efb13a65b754194ed
andersjr1984 <anderson.jasonryan@gmail.com>
2019-06-24 16:02:34 -0600

    got d3 to work with sample data

--- frontend/src/components/DefaultD3.js
+++ frontend/src/components/DefaultD3.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 import * as topojson from 'topojson';
 
+import * as unemploymentTsv from './tempData/unemployment.tsv'
+
 class DetaultD3 extends Component {
 
   constructor(props) {
@@ -13,19 +15,23 @@ class DetaultD3 extends Component {
     }
   }
 
-  componentWillMount() {
-    console.log("componentWillMount()")
+  async componentWillMount() {
+    console.log("componentWillMount()");
 
-    Promise.all(
-      [d3.json("https://d3js.org/us-10m.v1.json"),
-      d3.tsv("../static/unemployment.tsv")]
-    ).then( ([topologyData, unemploymentData]) => {
-          this.setState({
-            topologyData,
-            unemploymentData
-          })
-    }).catch(err => console.log('Error loading or parsing data.'))
-
+    try {
+      const topoLocation = "https://d3js.org/us-10m.v1.json";
+      const topologyData = await d3.json(topoLocation);
+  
+      const unemploymentData = await d3.tsv(unemploymentTsv);
+      console.log(unemploymentData)
+  
+      this.setState({
+        topologyData,
+        unemploymentData
+      })
+    } catch (error) {
+      console.log('Error loading or parsing data.')
+    }
   }
 
   componentDidUpdate() {
@@ -36,8 +42,11 @@ class DetaultD3 extends Component {
 
     const path = d3.geoPath();
     const unemployment = d3.map();
-
-    unemployment.set(this.state.unemploymentData.id, +this.state.unemploymentData.rate);
+    
+    //go through each county in the unemployment data and set the map
+    this.state.unemploymentData.forEach((countyData)=>{
+      unemployment.set(countyData.id, +countyData.rate);
+    })
 
     const x = d3.scaleLinear()
         .domain([1, 10])
@@ -59,13 +68,13 @@ class DetaultD3 extends Component {
           return d;
         }))
 
-    const usStates = topojson.feature(this.state.topologyData, this.state.topologyData.objects.states).features
+    const usCounties = topojson.feature(this.state.topologyData, this.state.topologyData.objects.counties).features
     const usStatePaths = topojson.mesh(this.state.topologyData, this.state.topologyData.objects.states, function(a, b) { return a !== b; });
 
     svg.append("g")
       .attr("class", "states")
       .selectAll("path")
-      .data(usStates)
+      .data(usCounties)
       .enter().append("path")
         .attr("fill", function(d) { return color(d.rate = unemployment.get(d.id)); })
         .attr("d", path)
