ca1363220c5f9815aa52c806d178c11bcc3cae15
Moses McCall <mmccall@mdpps.com>
2019-08-14 12:09:54 -0600

    update fixtures news app rawdata [ Note: since facility fixtures is large, it's zipped] added insert_location_pop_served management command

--- scripts/dump_test_data.sh
+++ scripts/dump_test_data.sh
@@ -5,3 +5,6 @@
 
 # dump test data for news
 ./manage.py dumpdata news.advisory_feed news.advisory_keyword news.alert news.url news.utility news.county_served --indent 4  > ./news/fixtures/test_data.json
+
+# dump raw data
+./manage.py dumpdata rawdata.EpaFacilitySystem --indent 4 > ./rawdata/fixtures/facility_system.json
