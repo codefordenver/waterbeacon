31c6c118dba07f43580de3256124477a5289f0ad
Derek Hassick <derek.hassick@gmail.com>
2020-02-24 22:45:56 -0500

    fixed whyWB comp styles and content

--- frontend/src/components/WhyWB.js
+++ frontend/src/components/WhyWB.js
@@ -5,9 +5,15 @@ import wbInfoLogo from '../images /SMWaterBeaconInfo.png'
 
 export default function WhyWB() {
   return (
-    <article id="why-wb">
-      <Image src={wbInfoLogo}/>
-      <h1>Why WB</h1>
+    <article id="why-wb" className="center-all">
+      <div className="center-all">
+        <h1>Why Water Beacon</h1>
+        <Image src={wbInfoLogo}/>
+      </div>
+      <div className="center-all">
+        <h3>Spreading knowledge and awareness on water quality.</h3>
+        <h3>For more information check out our Water Beacon Seminar <a>here</a></h3>
+      </div>
     </article>
   )
 }
