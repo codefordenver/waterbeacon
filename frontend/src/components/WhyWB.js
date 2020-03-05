9021de4df275ea66d8fefabcfebef1bf9f725743
Derek Hassick <derek.hassick@gmail.com>
2020-03-05 17:33:04 -0500

    why wb styled n completed

--- frontend/src/components/WhyWB.js
+++ frontend/src/components/WhyWB.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Container, Image } from 'react-bootstrap';
-import wbInfoLogo from '../images /SMWaterBeaconInfo.png'
+import { Image } from 'react-bootstrap';
+import wbInfoLogo from '../images/infoWB.png'
+import { NavHashLink } from 'react-router-hash-link';
 
 
 export default function WhyWB() {
   return (
     <article id="why-wb" className="center-all">
       <div className="center-all">
-        <h1>Why Water Beacon</h1>
+        <h1>Why Water Beacon?</h1>
         <Image src={wbInfoLogo}/>
       </div>
       <div className="center-all">
-        <h3>Spreading knowledge and awareness on water quality.</h3>
-        <h3>For more information check out our Water Beacon Seminar <a>here</a></h3>
+        <p>Water Beacon is to fill in the knowledge gap and lack of general awareness in the public. <br />
+          Making information about water quality data available to the public for easy access.
+        </p>
+        <p>For more in depth information check out our <NavHashLink to="/#workshop">Water Beacon Seminar</NavHashLink> below.</p>
       </div>
     </article>
   )
 }
+//  
