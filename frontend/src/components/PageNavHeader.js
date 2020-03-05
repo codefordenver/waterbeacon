9f9cbc23a44fd6ddad5eb96aab7fb28728c66525
Derek Hassick <derek.hassick@gmail.com>
2020-03-05 17:35:43 -0500

    fixed inherited nav movement

--- frontend/src/components/PageNavHeader.js
+++ frontend/src/components/PageNavHeader.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import { Image, Navbar, Nav, Button } from 'react-bootstrap';
 import wbSVG from '../WaterBeacon.svg';
-import wbLogo from '../images /waterBeaconLogo.png';
-import "./HeaderNav.css"
+import wbLogo from '../images/waterBeaconLogo.png';
+import './HeaderNav.css'
+import { NavHashLink } from 'react-router-hash-link';
 
 class PageNavHeader extends React.Component {
   render() {
     return (
-      //! need header for mobile version of header...
+    
+      //! get involved button needs link to the page/section
+
       <section className="page-header">
-        <Navbar className="nav-bar" bg="light"  expand="md" >
-          <Navbar.Brand href="#home">
-            <Image src={wbLogo} style={{ maxHeight: 110 }} fluid />
-            <Image src={wbSVG} />
+        <Navbar collapseOnSelect className="nav-bar" bg="light" style={{justifyContent: 'initial'}} expand="lg" >
+          <Navbar.Brand>
+            <Image src={wbLogo} style={{ maxHeight: 110 }} fluid alt="Water Beacon Logo picture"/>
+            <Image src={wbSVG} alt="Water Beacon Title"/>
           </Navbar.Brand>
-          <Nav className="mr-auto nav-buttons" >
-            <Nav.Link href="#why-wb">Why Water Beacon</Nav.Link>
-            <Nav.Link href="#community">Community</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto nav-buttons">
+              <NavHashLink to="/#why-wb" className="nav-items">
+                Why Water Beacon
+              </NavHashLink>
+              <NavHashLink to="/#workshop" className="nav-items">
+                Workshop
+              </NavHashLink>
+              <NavHashLink to="/#community" className="nav-items">
+                Community
+              </NavHashLink>
+              <NavHashLink to="/#partners" className="nav-items">
+                Partners
+              </NavHashLink>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <div>
           <Button> Get Involved</Button>
