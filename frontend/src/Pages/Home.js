fd9c9238a05603ee892521d020f610456d5c67a0
Derek Hassick <derek.hassick@gmail.com>
2020-03-06 14:44:52 -0500

    added all  components to hom page w/ no api call

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import WbHeaderBsNav from '../components/WbHeaderBsNav';
-import Retrieve from '../components/Retrieve';
+// import WbHeaderBsNav from '../components/WbHeaderBsNav';
+// import Retrieve from '../components/Retrieve';
 import PageNavHeader from '../components/PageNavHeader';
 import LiveData from '../components/LiveData';
 import './styleHome.css';
 import WhyWB from '../components/WhyWB';
+import WorkShop from '../components/WorkShop';
+import Partners from '../components/Partners';
+import Directions from '../components/Directions';
+import Community from '../components/Community';
+import Foot from '../components/Footer';
 
 class Home extends Component {
   render() {
@@ -13,8 +18,13 @@ class Home extends Component {
         <PageNavHeader />
         {/* <WbHeaderBsNav />
         <Retrieve /> */}
+        <Directions />
         <LiveData />
         <WhyWB />
+        <Community />
+        <WorkShop />
+        <Partners />
+        <Foot />
       </div>
     );
   }
