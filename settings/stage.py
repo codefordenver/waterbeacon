d12a175f8d36283b5077ccd303708ffecf8dd55a
Moses McCall <mosesmc52@gmail.com>
2020-04-19 12:21:04 -0600

    update settings configurations and added celery to docker

--- settings/stage.py
+++ settings/stage.py
@@ -1,17 +1,16 @@
 from common import *
 import raven
 
-DEBUG = False
-
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-EMAIL_BACKEND = 'sgbackend.SendGridBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
-SENDGRID_API_KEY = get_env_variable("SENDGRID_API_KEY",'')
-
+INSTALLED_APPS += ('raven.contrib.django.raven_compat',)
 RAVEN_CONFIG = {
     'dsn': 'https://9bcdaac1c2fd4f2585ed55da4e97140b:71dfb078e8e241f19b91cbb9342df14b@app.getsentry.com/73520',
     # If you are using git, you can also automatically configure the
     # release based on the git info.
     'release': raven.fetch_git_sha(BASE_DIR),
 }
+
+EMAIL_BACKEND = 'sgbackend.SendGridBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+SENDGRID_API_KEY = get_env_variable("SENDGRID_API_KEY",'')
