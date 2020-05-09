38099429619e3f46c911dd534b6bcb13656cbf2f
Moses McCall <mosesmc52@gmail.com>
2020-05-09 15:53:20 -0600

    fixed celerybeat issues

--- waterquality/celery.py
+++ waterquality/celery.py
@@ -4,9 +4,22 @@ import os
 from celery import Celery
 from django.conf import settings
 
-app = Celery('waterquality')
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+dotenv_path = os.path.join(BASE_DIR, ".env")
+load_dotenv(dotenv_path)
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.development')
+
+app = Celery('waterquality', broker = os.environ['CELERY_BROKER_URL'])
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
