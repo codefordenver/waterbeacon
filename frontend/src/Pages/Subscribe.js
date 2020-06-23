ea549a287aaaee40298acba27517abb30bffd141
Moses McCall <mosesmc52@gmail.com>
2020-06-22 19:40:27 -0600

    Subscribe Frontend page

---
+++ frontend/src/Pages/Subscribe.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import WbHeaderBsNav from '../components/WbHeaderBsNav';
+import Retrieve from '../components/Map/Retrieve';
+import PageNavHeader from '../components/PageNavHeader';
+import LiveData from '../components/LiveData';
+import './styleHome.css';
+
+class Subscribe extends Component {
+  render() {
+    return ();
+  }
+}
+
+export default Subscribe;
