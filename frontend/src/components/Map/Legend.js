ee77dbbb6516bc4aaa3f48003c440529d1d4a52a
andersjr1984 <anderson.jasonryan@gmail.com>
2021-02-25 00:03:59 -0700

    adding legend

--- frontend/src/components/Map/Legend.js
+++ frontend/src/components/Map/Legend.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import Minimize from '../../icons/Minimize'
 import * as R from 'ramda';
-import { Button } from 'react-bootstrap';
+import { colorScale } from './MapRender';
+
+const maxScore = 100;
+const scoreRange = [10, 30, 50, 95];
+const getColor = (score, maxScore) => {
+  console.log(score, maxScore);
+  return R.o(R.prop('color'), R.find(({ test }) => test(score, maxScore)))(colorScale);
+};
 
 const Legend = () => {
   const [showLegend, setShowLegend] = useState(true);
@@ -16,6 +23,17 @@ const Legend = () => {
           <svg width="14px" height="14px">
             <circle cx="7" cy="7" fill="red" r="7"/>
           </svg>
+          <p>You Are Here</p>
+          <svg width="14px" height="14px">
+            <circle cx="7" cy="7" fill="#67bf5c" r="7"/>
+          </svg>
+          <svg className="gradient" width={scoreRange.length * 20} height={14}>
+            {scoreRange.map((score, index) => <rect key={`legend${score}`} width={20} height={14} fill={getColor(score, maxScore)} x={ 20 * index }/>)}
+          </svg>
+          <div className="gradient words">
+            <span>Low Score</span>
+            <span>High Score</span>
+          </div>
         </>
       )}
     </div>
