83185c28eb0af4358572360c0fa816b41a4c3061
Jason Anderson <anderson.jasonryan@gmail.com>
2021-03-16 22:35:29 -0600

    updating api be

--- rawdata/admin.py
+++ rawdata/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from rawdata.models import EpaFacilitySystem
 
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(EpaFacilitySystem)
