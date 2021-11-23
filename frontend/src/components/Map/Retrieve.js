53df22cec4a0136271748e16d66d453483191fd2
Jason Anderson <anderson.jasonryan@gmail.com>
2021-11-22 22:09:56 -0700

    remove array

--- frontend/src/components/Map/Retrieve.js
+++ frontend/src/components/Map/Retrieve.js
@@ -3,29 +3,6 @@ import * as d3 from 'd3';
 import DefaultD3 from './DefaultD3';
 import * as R from 'ramda';
 
-const stateFipsId = {
-  "56":{State:"WY",},"54":{State:"WV",},"55":{State:"WI",},
-  "53":{State:"WA",},"50":{State:"VT",},"78":{State:"VI",},
-  "51":{State:"VA",},"49":{State:"UT",},"74":{State:"UM",},
-  "48":{State:"TX",},"47":{State:"TN",},"46":{State:"SD",},
-  "45":{State:"SC",},"44":{State:"RI",},"70":{State:"PW",},
-  "72":{State:"PR",},"42":{State:"PA",},"41":{State:"OR",},
-  "40":{State:"OK",},"39":{State:"OH",},"36":{State:"NY",},
-  "32":{State:"NV",},"35":{State:"NM",},"34":{State:"NJ",},
-  "33":{State:"NH",},"31":{State:"NE",},"38":{State:"ND",},
-  "37":{State:"NC",},"30":{State:"MT",},"28":{State:"MS",},
-  "69":{State:"MP",},"29":{State:"MO",},"27":{State:"MN",},
-  "26":{State:"MI",},"68":{State:"MH",},"23":{State:"ME",},
-  "24":{State:"MD",},"25":{State:"MA",},"22":{State:"LA",},
-  "21":{State:"KY",},"20":{State:"KS",},"18":{State:"IN",},
-  "17":{State:"IL",},"16":{State:"ID",},"19":{State:"IA",},
-  "15":{State:"HI",},"66":{State:"GU",},"13":{State:"GA",},
-  "64":{State:"FM",},"12":{State:"FL",},"10":{State:"DE",},
-  "11":{State:"DC",},"09":{State:"CT",},"08":{State:"CO",},
-  "06":{State:"CA",},"04":{State:"AZ",},"60":{State:"AS",},
-  "05":{State:"AR",},"01":{State:"AL",},"02":{State:"AK",}
-};
-
 // this component will retrieve the data from our database
 const Retrieve = () => {
   // this is the data used to build the map
