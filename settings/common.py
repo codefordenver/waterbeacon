007b062a9a475b12404d86f01f775ad95de72d34
Moses McCall <mosesmc52@gmail.com>
2021-12-23 20:24:38 -0700

    update backend static path to django-static

--- settings/common.py
+++ settings/common.py
@@ -127,7 +127,7 @@ USER= get_env_variable('USER')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django-static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 CELERY_RESULT_BACKEND = 'django-db'
