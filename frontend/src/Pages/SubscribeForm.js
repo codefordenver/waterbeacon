5187dd1b85c4263367df9fa69b5521978392e7b5
Moses McCall <mosesmc52@gmail.com>
2021-12-29 08:19:42 -0600

    no message

--- frontend/src/Pages/SubscribeForm.js
+++ frontend/src/Pages/SubscribeForm.js
@@ -26,11 +26,12 @@ const SubscribeForm = () => {
   const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
 
   const getCookie = (name) => {
-      var cookieValue = null;
+      let cookieValue = null;
       if (document.cookie && document.cookie !== '') {
-          var cookies = document.cookie.split(';');
-          for (var i = 0; i < cookies.length; i++) {
-              var cookie = jQuery.trim(cookies[i]);
+          const cookies = document.cookie.split(';');
+          for (let i = 0; i < cookies.length; i++) {
+              const cookie = cookies[i].trim();
+              // Does this cookie string begin with the name we want?
               if (cookie.substring(0, name.length + 1) === (name + '=')) {
                   cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                   break;
