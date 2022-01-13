6cc1da658d9ce7485261d16ffe32a88b610a4295
Jason Anderson <anderson.jasonryan@gmail.com>
2022-01-13 16:36:50 -0700

    change table

--- frontend/src/components/Map/DefaultD3.js
+++ frontend/src/components/Map/DefaultD3.js
@@ -182,7 +182,7 @@ const TopCounties = (props) => {
   };
 
   return (
-    <Table striped bordered hover variant="dark" size="sm" className="county-list">
+    <Table striped bordered hover variant="light" size="sm" className="county-list">
       <thead>
         <tr>
           <th>
@@ -215,7 +215,7 @@ const TopCounties = (props) => {
               <td>
                 {county.state}
               </td>
-              <td>
+              <td style={{textAlign: "center"}}>
                 {Math.floor(county.score * 100) + '%'}
               </td>
               <td className="remove-county" onClick={()=>removeCounty(index)}>
