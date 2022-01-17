e8063737f7abaabe94827ddc8721df25596ec095
Moses McCall <mosesmc52@gmail.com>
2022-01-16 21:37:36 -0600

    Fixed closing tag

--- frontend/src/components/WorkShop.js
+++ frontend/src/components/WorkShop.js
@@ -10,7 +10,7 @@ export default function WorkShop() {
     <Container>
       <Row >
         <Col >
-          <h2>Workshop</h12>
+          <h2>Workshop</h2>
           <p class="text-muted">We organize workshops to inform you the basics of water quality and how you can navigate the wealth of data and find meaningful revelations about the state of water quality where you live. We also discuss with you how we can make this vision a reality. Click to subscribe to our mailing to be notified of our next workshop.</p>
         </Col>
         <Col>
