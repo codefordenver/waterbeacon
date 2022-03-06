3db5630f17a06f492b244d252f217f7ef2f10672
Moses McCall <mosesmc52@gmail.com>
2022-03-06 10:58:47 -0600

    no message

--- frontend/src/components/Map/DefaultD3.js
+++ frontend/src/components/Map/DefaultD3.js
@@ -117,7 +117,7 @@ const DefaultD3 = ({
                   className={ 'rounded-0 mr-10'}
                   disabled={isCurrentSelection || !existing}
                   variant={variant + 'primary'}
-                  onClick={() => updateChosenPeriod(quarterOption)}
+                  onClick={() => updateChosenPeriod(index)}
                   key={value}
                 >
                   {value}
