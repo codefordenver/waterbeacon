76e665e596dd0f0cf192e0583cde1907b2d1af90
Moses McCall <mosesmc52@gmail.com>
2022-03-06 10:27:15 -0600

    no message

--- frontend/src/components/Map/MapRender.js
+++ frontend/src/components/Map/MapRender.js
@@ -79,7 +79,8 @@ export const MapRender = (props) => {
     }
     const translateData = () => {
       //set the svg to the anchor element
-      svg.current = d3.select(anchor.current).remove().append("svg")
+      d3.select(anchor.current).remove()
+      svg.current = d3.select(anchor.current).append("svg")
         .attr("viewBox", `0 0 ${width} ${height}`)
         .attr("preserveAspectRatio", "xMidYMin meet")
         .attr("height", "100%")
