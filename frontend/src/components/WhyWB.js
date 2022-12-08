55d8167effc082897024988d68050b6adcbdb915
Moses McCall <mosesmc52@gmail.com>
2022-12-08 18:42:04 -0500

    created templates for water quality reports

--- frontend/src/components/WhyWB.js
+++ frontend/src/components/WhyWB.js
@@ -7,12 +7,12 @@ export default function WhyWB() {
   return (
     <article id="why-wb" className="center-all">
       <div className="center-all">
-        <h2 style={{ color: 'white'}}>Why Water Beacon?</h2>
+        <h2 style={{ color: 'white'}}>Why?</h2>
         <Image src={wbInfoLogo} style={{ maxHeight: 100, padding:10 }}/>
       </div>
       <div className="center-all">
         <p>
-             The vision of Water Beacon is to make drinking water quality data easy to understand.
+             The purpose of Water Beacon is to make drinking water quality data easy to understand.
         </p>
       </div>
     </article>
--- settings/common.py
+++ settings/common.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from datetime import timedelta
-from dotenv import load_dotenv
+
 import dj_database_url
 import sentry_sdk
+from dotenv import load_dotenv
 from sentry_sdk.integrations.django import DjangoIntegration
 
 from . import get_env_variable
@@ -24,71 +25,71 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 dotenv_path = os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path)
 
-from utils.utils import ( str2bool )
+from utils.utils import str2bool
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r(*#1+d-n2vma&*bg6$knhjbiy#82-_5_2vz9z=&$#eh%y9en6'
+SECRET_KEY = "r(*#1+d-n2vma&*bg6$knhjbiy#82-_5_2vz9z=&$#eh%y9en6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str2bool(get_env_variable('DEBUG', True))
+DEBUG = str2bool(get_env_variable("DEBUG", True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.gis',
-    'rest_framework',
-    'django_celery_results',
-    'django_celery_beat',
-    'admin_auto_filters',
-    'app',
-    'news',
-    'rawdata',
-    'subscribe'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.gis",
+    "rest_framework",
+    "django_celery_results",
+    "django_celery_beat",
+    "admin_auto_filters",
+    "app",
+    "news",
+    "rawdata",
+    "subscribe",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'waterquality.urls'
+ROOT_URLCONF = "waterquality.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates/')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates/")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'waterquality.wsgi.application'
+WSGI_APPLICATION = "waterquality.wsgi.application"
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
 
 
 # Password validation
@@ -96,16 +97,16 @@ DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -113,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -123,48 +124,49 @@ USE_L10N = True
 
 USE_TZ = True
 
-ENVIRONMENT = get_env_variable('ENVIRONMENT')
-USER= get_env_variable('USER')
+ENVIRONMENT = get_env_variable("ENVIRONMENT")
+USER = get_env_variable("USER")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/django-static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/django-static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # celery configuration
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_BROKER_URL = get_env_variable('CELERY_BROKER_URL')
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'America/Denver'
+CELERY_RESULT_BACKEND = "django-db"
+CELERY_BROKER_URL = get_env_variable("CELERY_BROKER_URL")
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = "America/Denver"
 
 
 # Email Configurations
-TEMPLATED_EMAIL_BACKEND = 'templated_email.backends.vanilla_django'
-TEMPLATED_EMAIL_TEMPLATE_DIR = 'templated_email/' #use '' for top level template dir, ensure there is a trailing slash
-TEMPLATED_EMAIL_FILE_EXTENSION = 'email'
-EMAIL_FROM = get_env_variable('EMAIL_FROM','')
-SUPPORT_EMAIL = get_env_variable('SUPPORT_EMAIL','')
+TEMPLATED_EMAIL_BACKEND = "templated_email.backends.vanilla_django"
+TEMPLATED_EMAIL_TEMPLATE_DIR = "templated_email/"  # use '' for top level template dir, ensure there is a trailing slash
+TEMPLATED_EMAIL_FILE_EXTENSION = "email"
+EMAIL_FROM = get_env_variable("EMAIL_FROM", "")
+SUPPORT_EMAIL = get_env_variable("SUPPORT_EMAIL", "")
 
-if  get_env_variable('AWS_SES_ACCESS_KEY_ID', None):
-    EMAIL_BACKEND = 'django_ses.SESBackend'
-    AWS_SES_ACCESS_KEY_ID = get_env_variable('AWS_SES_ACCESS_KEY_ID')
-    AWS_SES_SECRET_ACCESS_KEY = get_env_variable('AWS_SES_SECRET_ACCESS_KEY')
-    AWS_SES_REGION_NAME=get_env_variable('AWS_SES_REGION_NAME')
-    AWS_SES_REGION_ENDPOINT = get_env_variable('AWS_SES_REGION_ENDPOINT')
+if get_env_variable("AWS_SES_ACCESS_KEY_ID", None):
+    EMAIL_BACKEND = "django_ses.SESBackend"
+    AWS_SES_ACCESS_KEY_ID = get_env_variable("AWS_SES_ACCESS_KEY_ID")
+    AWS_SES_SECRET_ACCESS_KEY = get_env_variable("AWS_SES_SECRET_ACCESS_KEY")
+    AWS_SES_REGION_NAME = get_env_variable("AWS_SES_REGION_NAME")
+    AWS_SES_REGION_ENDPOINT = get_env_variable("AWS_SES_REGION_ENDPOINT")
 else:
-    TEMPLATED_EMAIL_BACKEND = 'templated_email.backends.vanilla_django.TemplateBackend'
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+    TEMPLATED_EMAIL_BACKEND = "templated_email.backends.vanilla_django.TemplateBackend"
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 MAXIMUM_CHART_DAYS = timedelta(days=2)
 
 # where the EPA data requests are stored in json files before being imported into the DB
-EPA_DATA_DIRECTORY = 'rawdata/epa/'
+EPA_DATA_DIRECTORY = "rawdata/epa/"
 
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ["*"]
